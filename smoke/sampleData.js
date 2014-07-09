b4e85f9bed34fc61d2c1ec510aff61f7ba76cd56
Rob Beers <rbeers@bhawke.com>
2014-07-09 11:01:29 -0400

    added global filters and ability to interact with analytics

--- smoke/sampleData.js
+++ smoke/sampleData.js
@@ -5,13 +5,22 @@
             name: 'Solr Dashboard',
             id: 'food',
             theme: 'green',
+            global: {
+                query: {
+                    where: [
+                        {field:'cat',op:'equals',value:null},
+                        {field:'manu_id_s',op:'equals',value:null}
+                    ]
+                }
+            },
             analytics: [
                 {
                     type: 'solr',
                     name: 'By Category',
-                    baseUrl: 'http://cloudfortressinc.com:8983/solr/',
+                    baseUrl: 'http://localhost:8983/solr/',
                     chartType: 'Bar',
                     limit: 8,
+                    target:'cat',
                     query: {
                         select: [{expr:'cat'}],
                         from: 'collection1'
@@ -20,9 +29,10 @@
                 {
                     type: 'solr',
                     name: 'By Manufacturer',
-                    baseUrl: 'http://cloudfortressinc.com:8983/solr/',
+                    baseUrl: 'http://localhost:8983/solr/',
                     chartType: 'Doughnut',
                     limit: 8,
+                    target:'manu_id_s',
                     query: {
                         select: [{expr:'manu_id_s'}],
                         from: 'collection1'
